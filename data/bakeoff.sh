fcdf68fb43494e279592144fd246fd0937ca5117
Glenn Hickey <glenn.hickey@gmail.com>
2015-11-10 13:48:44 -0500

    add snp merge option to bakeoff script

--- data/bakeoff.sh
+++ data/bakeoff.sh
@@ -3,19 +3,25 @@
 #set -e
 
 ASSEMBLY="GRCh38"
+WINDOW=100
 
 #get the vcf file associated with a region
 function get_region_vcf {
 
 	 REGION=$1
+	 MERGE=$2
 
 	 # get the bed path
 	 BED=${REGION}_${ASSEMBLY}/${REGION}.bed
 
 	 # get contig
 	 CONTIG=`cat ${BED} | awk '{print $1}'`
-
-	 echo "vcf_${ASSEMBLY}/${CONTIG}.vcf.gz"
+	 if [ $MERGE != "1" ]
+	 then
+		  echo "vcf_${ASSEMBLY}/${CONTIG}.vcf.gz"
+	 else
+		  echo "vcf_${ASSEMBLY}/${CONTIG}_merge_${WINDOW}.vcf.gz"
+	 fi
 }
 
 # get the fasta file associated with a region
@@ -54,13 +60,32 @@ function get_region_coords {
 
 for REGION in BRCA1 BRCA2 MHC SMA LRC_KIR
 do
+	 FA_FILE=`get_region_fa ${REGION}`
+	 VCF_FILE=`get_region_vcf ${REGION}`
+	 COORDS=`get_region_coords ${REGION}`
+
 	 if [ ! -e "${REGION}_${ASSEMBLY}/${REGION}.vg" ]
 	 then
 		  ./fetch1kgpRegion.py ${REGION} --assembly ${ASSEMBLY}
-		  FA_FILE=`get_region_fa ${REGION}`
-		  VCF_FILE=`get_region_vcf ${REGION}`
-		  COORDS=`get_region_coords ${REGION}`
 		  vg construct -r ${FA_FILE} -R ${COORDS} -v ${VCF_FILE} -p > ${REGION}_${ASSEMBLY}/${REGION}.vg
+	 fi
+
+	 if [ ! -e "${REGION}_${ASSEMBLY}/${REGION}_merge_${WINDOW}.vg" ]
+	 then
+		  ./fetch1kgpRegion.py ${REGION} --assembly ${ASSEMBLY}
+		  MERGE_VCF_FILE=`get_region_vcf ${REGION} 1`
+		  if [ ! -e "${$MERGE_VCF_FILE}" ]
+		  then
+				# merge up blocks of phased snps into their own haplotypes
+				gzip -d -c ${VCF_FILE} | vcfgeno2haplo -r ${FA_FILE} -w ${WINDOW} > ${MERGE_VCF_FILE}
+		  fi
+		  
+		  # create compressed vcf
+		  bgzip -f ${MERGE_VCF_FILE}
+		  tabix -f -p vcf ${MERGE_VCF_FILE}.gz
+
+		  # make vg with -f (keep alleles)
+		  vg construct -f -r ${FA_FILE} -R ${COORDS} -v ${VCF_FILE} -p > ${REGION}_${ASSEMBLY}/${REGION}_merge_${WINDOW}.vg
 	 fi
 done
 
