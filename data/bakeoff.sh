1cc3d9bccb53792f5daa2ad011843daa215bcde7
Glenn Hickey <hickey@soe.ucsc.edu>
2015-11-11 06:58:09 -0800

    add snp merging to bakeoff script.  needsto be sped up

--- data/bakeoff.sh
+++ data/bakeoff.sh
@@ -61,20 +61,23 @@ function get_region_coords {
 for REGION in BRCA1 BRCA2 MHC SMA LRC_KIR
 do
 	 FA_FILE=`get_region_fa ${REGION}`
-	 VCF_FILE=`get_region_vcf ${REGION}`
+	 VCF_FILE=`get_region_vcf ${REGION} 0`
 	 COORDS=`get_region_coords ${REGION}`
+
+	 if [[ ! -e ${VCF_FILE} ]] || [[ ! -e ${FA_FILE} ]]
+	 then
+		  ./fetch1kgpRegion.py ${REGION} --assembly ${ASSEMBLY}
+	 fi
 
 	 if [ ! -e "${REGION}_${ASSEMBLY}/${REGION}.vg" ]
 	 then
-		  ./fetch1kgpRegion.py ${REGION} --assembly ${ASSEMBLY}
 		  vg construct -r ${FA_FILE} -R ${COORDS} -v ${VCF_FILE} -p > ${REGION}_${ASSEMBLY}/${REGION}.vg
 	 fi
 
 	 if [ ! -e "${REGION}_${ASSEMBLY}/${REGION}_merge_${WINDOW}.vg" ]
 	 then
-		  ./fetch1kgpRegion.py ${REGION} --assembly ${ASSEMBLY}
 		  MERGE_VCF_FILE=`get_region_vcf ${REGION} 1`
-		  if [ ! -e "${$MERGE_VCF_FILE}" ]
+		  if [ ! -e "${MERGE_VCF_FILE}" ]
 		  then
 				# merge up blocks of phased snps into their own haplotypes
 				gzip -d -c ${VCF_FILE} | vcfgeno2haplo -r ${FA_FILE} -w ${WINDOW} > ${MERGE_VCF_FILE}
@@ -95,5 +98,10 @@ do
 	 then
 		  vg2sg ${REGION}_${ASSEMBLY}/${REGION}.vg ${REGION}_${ASSEMBLY}/database.fa ${REGION}_${ASSEMBLY}/database.sql -s
 	 fi
+	 if [ ! -e "${REGION}_${ASSEMBLY}/database_merge_${WINDOW}.sql" ]
+	 then
+		  vg2sg ${REGION}_${ASSEMBLY}/${REGION}_merge_${WINDOW}.vg ${REGION}_${ASSEMBLY}/database_merge_${WINDOW}.fa ${REGION}_${ASSEMBLY}/database_merge_${WINDOW}.sql -s
+	 fi
+
 done
 
